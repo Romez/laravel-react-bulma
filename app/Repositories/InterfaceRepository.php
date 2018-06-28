c977c02052395dd7993f4a3346bb8bda0ec3b09f
roman.ushakov <roman.ushakov.89@inbox.ru>
2018-06-28 06:30:18 +0300

    create repository

---
+++ app/Repositories/InterfaceRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+interface InterfaceRepository
+{
+    /**
+     * @param string $select
+     * @param bool $where
+     * @return mixed
+     */
+    public function get($select = '*', $where = false);
+
+    /**
+     * @param array $params
+     * @return mixed
+     */
+    public function one(array $params);
+
+    /**
+     * @param int $id
+     * @param bool $withTrashed
+     * @return Model|null
+     */
+    public function getById($id, $withTrashed = false);
+
+    /**
+     * @param array $data
+     * @return Model
+     */
+    public function create(array $data): Model;
+
+    /**
+     * @param int $id
+     * @param array $data
+     * @return mixed
+     */
+    public function update(int $id, array $data);
+
+    /**
+     * @param array $data
+     * @return mixed
+     */
+    public function insert(array $data);
+
+    /**
+     * @param int $id
+     * @return bool
+     */
+    public function delete(int $id);
+}
---
+++ app/Repositories/Repository.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
+
+abstract class Repository implements InterfaceRepository
+{
+
+    /** @var Model */
+    protected $model;
+
+    /**
+     * @return Model;
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * @param string $select
+     * @param bool $where
+     * @return Collection
+     */
+    public function get($select = '*', $where = false)
+    {
+        $builder = $this->model->select($select);
+
+        if ($where) {
+            $builder->where($where);
+        }
+
+        return $builder->get();
+    }
+
+    /**
+     * @param $params
+     * @return Model|null|static
+     */
+    public function one(array $params)
+    {
+        return $this->model->where($params)->first();
+    }
+
+    /**
+     * @param  int $id
+     * @param  bool $withTrashed
+     * @return Model
+     */
+    public function getById($id, $withTrashed = false)
+    {
+        if ($withTrashed) {
+            return $this->model->withTrashed()->findOrFail($id);
+        }
+
+        return $this->model->findOrFail($id);
+    }
+
+    /**
+     * @param array $data
+     * @return Model
+     */
+    public function create(array $data): Model
+    {
+        return $this->model->create($data);
+    }
+
+    /**
+     * @param int $id
+     * @param array $data
+     * @return mixed
+     */
+    public function update(int $id, array $data)
+    {
+        return $this->model->findOrFail($id)->update($data);
+    }
+
+
+    /**
+     * @param array $data
+     * @return mixed
+     */
+    public function insert(array $data)
+    {
+        return $this->model->insert($data);
+    }
+
+    /**
+     * @param int $id
+     * @return bool|null
+     * @throws \Exception
+     */
+    public function delete(int $id)
+    {
+        return $this->model->delete($id);
+    }
+}
